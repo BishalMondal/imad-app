3ac2a17607fb45ff70ff525b2dc16bbf5ebe306f
Bishal Mondal <bishalmondal2015@gmail.com>
2017-08-16 23:33:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,5 +16,5 @@ function moveRight(){
 
 img.onclick = function() {
     //img.style.marginLeft = '100px';
-    var interval = setInterval(moveRight, 45);
+    var interval = setInterval(moveRight, 50);
 };
