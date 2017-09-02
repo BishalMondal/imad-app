910a1194d9d154102d54433677ce910e63c15bcd
Bishal Mondal <bishalmondal2015@gmail.com>
2017-09-02 19:25:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.get('/articles/:articleName', function(req, res){
     //articleName = Article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
-//SELECT * FROM article WHERE title = 'Article-one'
+    //SELECT * FROM article WHERE title = 'Article-one'
     pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'" , function(err, result){
         if(err){
             res.status(500).send(err.toString());
