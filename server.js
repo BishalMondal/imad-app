720703157d289b821d2cdc181bc4920b80bdfedb
Bishal Mondal <bishalmondal2015@gmail.com>
2017-09-02 17:49:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,16 +115,14 @@ var pool = new Pool(config);
 app.get('/test-db', function(req, res) {
     //make a select request
     //return a response withthe result
-    pool.query('SELECT * FROM test', function(err, res){
+    pool.query('SELECT * FROM test', function(err, result){
         if(err){
             res.status(500).send(err.toString());
-        }
-        else{
+        }else{
             res.send(JSON.stringify(result));
         }
     
-    
-});
+    });
 });
 
 var counter = 0;
